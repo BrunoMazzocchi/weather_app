9fe0ce407883e780e593ca404da17ef24ec828d5
Bruno Mazzocchi <bemazzocchi@gmail.com>
2022-11-23 19:24:05 -0600

    Initial commit:

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
+        home: Scaffold(
+          body: Container (
+            child: Center(
+              child: Text("Weather app"),
+            ),
+          ),
+        )
+    );
+  }}
