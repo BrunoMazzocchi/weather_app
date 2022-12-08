f161258bc60fa57ea117c7f9459ee76db6502ff3
Bruno Mazzocchi <bemazzocchi@gmail.com>
2022-12-07 20:31:22 -0600

    Commit

--- lib/resources/forecast_weather_api_provider.dart
+++ lib/resources/forecast_weather_api_provider.dart
@@ -3,13 +3,12 @@ import 'package:dio/dio.dart';
 import '../models/forecast_weather/forecast_weather_model.dart';
 
 class ForecastWeatherApiProvider {
-  final key = '';
   final Dio _dio = Dio();
 
   Future<ForecastWeather> fetchForecastWeather(String coord) async {
     try {
       Response response = await _dio.get(
-          'https://api.openweathermap.org/data/3.0/onecall?appid=0472b4b25ca6f14090f7c846ef25e025&lat=35.6895&lon=139.6917&exclude=hourly,minutely');
+          'https://api.openweathermap.org/data/3.0/onecall?appid=&lat=35.6895&lon=139.6917&exclude=hourly,minutely');
       return ForecastWeather.fromJson(response.data);
     } catch (error, stacktrace) {
       throw Exception('Failed to load weather');
